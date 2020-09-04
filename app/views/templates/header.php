fcaa7b052ae9310189ad7272a7f1b0d70c3eaca8
Alfan <alfan.wahyudi98@gmail.com>
2020-09-04 14:15:53 +0700

    fix bug for header

--- app/views/templates/header.php
+++ app/views/templates/header.php
@@ -42,14 +42,14 @@
     <header id="header" class="fixed-top ">
         <div class="container d-flex align-items-center">
 
-            <h1 class="logo"><a href="index.html">Sailor</a></h1>
+            <h1 class="logo"><a href="<?= BASEURL; ?>/home">Sailor</a></h1>
             <!-- Uncomment below if you prefer to use an image logo -->
             <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
             <nav class="nav-menu d-none d-lg-block">
 
                 <ul>
-                    <li class="active"><a href="index.html">Home</a></li>
+                    <li class="active"><a href="<?= BASEURL; ?>/home">Home</a></li>
 
                     <li class="drop-down"><a href="#">About</a>
                         <ul>
