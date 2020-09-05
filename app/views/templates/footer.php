0ec996ae0ca72130f91a8a1642329937b074d2f7
Alfan <alfan.wahyudi98@gmail.com>
2020-09-05 21:05:46 +0700

    delete link to css and javascript in header and footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -71,22 +71,3 @@
          </div>
      </div>
  </footer><!-- End Footer -->
-
- <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
-
- <!-- Vendor JS Files -->
- <script src="<?= BASEURL; ?>/public/vendor/jquery/jquery.min.js"></script>
- <script src="<?= BASEURL; ?>/public/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
- <script src="<?= BASEURL; ?>/public/vendor/jquery.easing/jquery.easing.min.js"></script>
- <script src="<?= BASEURL; ?>/public/vendor/php-email-form/validate.js"></script>
- <script src="<?= BASEURL; ?>/public/vendor/isotope-layout/isotope.pkgd.min.js"></script>
- <script src="<?= BASEURL; ?>/public/vendor/venobox/venobox.min.js"></script>
- <script src="<?= BASEURL; ?>/public/vendor/waypoints/jquery.waypoints.min.js"></script>
- <script src="<?= BASEURL; ?>/public/vendor/owl.carousel/owl.carousel.min.js"></script>
-
- <!-- Template Main JS File -->
- <script src="<?= BASEURL; ?>/public/js/main.js"></script>
-
- </body>
-
- </html>
