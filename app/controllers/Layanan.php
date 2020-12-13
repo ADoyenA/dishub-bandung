30eec34a5fd43340997a67eafd0525e223c4d14c
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2020-12-13 13:54:57 +0700

    Add Layanan Page

---
+++ app/controllers/Layanan.php
@@ -0,0 +1,14 @@
+<?php
+
+class Layanan extends controller
+{
+    public function index()
+    {
+        $data['judul'] = 'Dishub Bandung';
+        $this->view('templates/beforeHeader', $data);
+        $this->view('templates/header', $data);
+        $this->view('about/index', $data);
+        $this->view('templates/footer', $data);
+        $this->view('templates/afterFooter', $data);
+    }
+}
