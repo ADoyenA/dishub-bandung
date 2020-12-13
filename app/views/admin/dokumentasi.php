04b5b9769f69a638dbe7346395c8b56f7c296d70
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2020-12-13 20:35:39 +0700

    delete breadcrums in dashboard

--- app/views/admin/dokumentasi.php
+++ app/views/admin/dokumentasi.php
@@ -1,18 +1,4 @@
 <main>
-    <!-- ======= Breadcrumbs ======= -->
-    <section id="breadcrumbs" class="breadcrumbs">
-        <div class="container">
-
-            <div class="d-flex justify-content-between align-items-center">
-                <h2>Dokumentasi</h2>
-                <ol>
-                    <li><a href="<?= BASEURL; ?>/dashboard">Dashboard</a></li>
-                    <li>Dokumentasi</li>
-                </ol>
-            </div>
-
-        </div>
-    </section><!-- End Breadcrumbs -->
     <!-- ======= Services Section ======= -->
     <section id="services" class="services">
         <div class="container" data-aos="fade-up">
