7f028b7f4af70048fc8f45077b8c9dae0d9c0120
Dida Handian <handiandida@gmail.com>
2021-01-27 23:23:03 +0700

    rapih rapih

--- app/views/kelembagaan/sekretariat.php
+++ app/views/kelembagaan/sekretariat.php
@@ -21,8 +21,7 @@
 
             <div class="row no-gutters">
                 <div class="col-lg-6 video-box">
-                    <img src="<?= BASEURL; ?>/public/img/about.jpg" class="img-fluid" alt="">
-                    <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+                    <img src="<?= BASEURL; ?>/public/img/sekretariat.png" class="img-fluid" alt="">
                 </div>
 
                 <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
--- app/views/kelembagaan/unit_pelaksana_teknis.php
+++ app/views/kelembagaan/unit_pelaksana_teknis.php
@@ -21,8 +21,7 @@
 
             <div class="row no-gutters">
                 <div class="col-lg-6 video-box">
-                    <img src="<?= BASEURL; ?>/public/img/about.jpg" class="img-fluid" alt="">
-                    <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+                    <img src="<?= BASEURL; ?>/public/img/angkutan.png" class="img-fluid" alt="">
                 </div>
 
                 <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
@@ -48,8 +47,7 @@
 
             <div class="row no-gutters">
                 <div class="col-lg-6 video-box">
-                    <img src="<?= BASEURL; ?>/public/img/about.jpg" class="img-fluid" alt="">
-                    <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+                    <img src="<?= BASEURL; ?>/public/img/parkir.png" class="img-fluid" alt="">
                 </div>
 
                 <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
@@ -75,8 +73,7 @@
 
             <div class="row no-gutters">
                 <div class="col-lg-6 video-box">
-                    <img src="<?= BASEURL; ?>/public/img/about.jpg" class="img-fluid" alt="">
-                    <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="venobox play-btn mb-4" data-vbtype="video" data-autoplay="true"></a>
+                    <img src="<?= BASEURL; ?>/public/img/terminal.png" class="img-fluid" alt="">
                 </div>
 
                 <div class="col-lg-6 d-flex flex-column justify-content-center about-content">
